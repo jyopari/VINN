e1c288f132d3a3682161114c4aec9c8c0fe6b0bc
James Powell <james@dontusethiscode.com>
2021-12-04 23:23:04 -0500

    example refactor (NOTE: incomplete; code will not run!)

--- train_BC.py
+++ train_BC.py
@@ -1,69 +1,64 @@
 import sys
 import numpy as np
 import argparse
+from pathlib import Path
+from enum import Enum
+from collections import namedtuple
+from imitation_models import BC
+from imitation_models import BC_Full
+
+ModelDef = namedtuple('ModelDef', 'fruc fracs')
+class Model(Enum):
+    Rep = ModelDef(BC, [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    EndToEnd = ModelDef(BC_Full, [0.05, 0.1, 0.3, 0.7, 1.0])
+    func = property(lambda s: s.value.func)
+    fracs = property(lambda s: s.value.fracs)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int)
-parser.add_argument('--root_dir', type=str)
-parser.add_argument('--gpu', type=int)
+parser.add_argument('--root_dir', type=Path)
+parser.add_argument('--gpu', type=bool)
 parser.add_argument('--img_size', type=int)
-parser.add_argument('--train_dir', type=str)
-parser.add_argument('--val_dir', type=str)
-parser.add_argument('--test_dir', type=str)
+parser.add_argument('--train_dir', type=Path)
+parser.add_argument('--val_dir', type=Path)
+parser.add_argument('--test_dir', type=Path)
 parser.add_argument('--layer', type=str)
-parser.add_argument('--dataset', type=str)
-parser.add_argument('--representation_model_path', type=str)
+parser.add_argument('--dataset', type=str, choices=['push', 'handle', 'stack'])
+parser.add_argument('--representation_model_path', type=Path)
 parser.add_argument('--model', type=str)
-parser.add_argument('--wandb', type=int)
+parser.add_argument('--wandb', type=bool)
 parser.add_argument('--lr', type=float)
 parser.add_argument('--epochs', type=int)
-parser.add_argument('--save_dir', type=str)
-parser.add_argument('--architecture', type=str)
+parser.add_argument('--save_dir', type=Path)
+parser.add_argument('--architecture', type=str, choices=['ResNet', 'AlexNet'])
 parser.add_argument('--eval', type=int)
-parser.add_argument('--temporal', type=int)
+parser.add_argument('--temporal', type=bool)
 parser.add_argument('--t', type=int)
-parser.add_argument('--bc_model', type=str, choices=['BC_rep', 'BC_end_to_end'])
-parser.add_argument('--pretrain_encoder', type=int)
+parser.add_argument('--bc_model', type=Model.__getitem__, default=Model.Rep)
+parser.add_argument('--pretrain_encoder', type=bool)
 parser.add_argument('--pretrained', type=int)
 parser.add_argument('--partial', type=float)
 
-args = parser.parse_args()
-params = vars(args)
+def main(params):
+    bc = args.bc_model.func(params)
 
-sys.path.append(params['root_dir'] + 'imitation_models')
-from BC import BC
-from BC_full import BC_Full
+    losses = array([
+        bc.get_val_losses(p, 5) if params.dataset == 'handle' else bc.get_test_losses(p, 5)
+        for p in args.bc_model.fracs:
+    ])
 
-def run_bc_model(params):
-    all_losses = []
-    all_means = []
-    all_stds = []
-
-    if(params['bc_model'] == 'BC_rep'):
-        fractions = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-        bc = BC(params)
-    elif(params['bc_model'] == 'BC_end_to_end'):
-        fractions = [0.05, 0.1, 0.3, 0.7, 1.0]
-        bc = BC_Full(params)
-
-    for p in fractions:
-        losses = []
-        if params['dataset'] == 'HandleData':
-            losses = bc.get_val_losses(p, 5)
-        else:
-            losses = bc.get_test_losses(p, 5)
-        losses = np.array(losses)
-        mean = np.mean(losses)
-        std = np.std(losses)
-        print(p, mean, std)
-        all_losses.append(losses)
-        all_means.append(mean)
-        all_stds.append(std)
-
-    suffix = f"{params['dataset']}_{params['pretrained']}.txt"
-    np.savetxt(f"../results/{params['bc_model']}_losses_{suffix}", np.array(all_losses), delimiter=",")
-    np.savetxt(f"../results/{params['bc_model']}_means_{suffix}", np.array(all_means), delimiter=",")
-    np.savetxt(f"../results/{params['bc_model']}_stds_{suffix}", np.array(all_stds), delimiter=",")
+    results_dir = Path('../results')
+    stem = '_'.join([params.bc_model.name, params.dataset, params.pretrained])
+    paths = {
+        'losses': results_dir / stem.with_suffix('.losses.txt')
+        'means':  results_dir / stem.with_suffix('.means.txt')
+        'stds':   results_dir / stem.with_suffix('.stds.txt')
+    }
+    np.savetxt(paths['losses'], losses, delimiter=',')
+    np.savetxt(paths['means'], losses.mean(axis=-1), delimiter=',')
+    np.savetxt(paths['stds'], losses.std(axis=-1), delimiter=',')
 
 if __name__ == '__main__':
-    run_bc_model(params)
+    args = parser.parse_args()
+    # sys.path.append(params['root_dir'] / 'imitation_models') # XXX: why?
+    main(args)
